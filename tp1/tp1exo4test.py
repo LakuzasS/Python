d4507ccd411ec0f2efb2508f3496634661c1abf0
LakuzasS <104200455+LakuzasS@users.noreply.github.com>
2022-12-22 12:20:34 +0100

    Delete tp1exo4test.py

--- tp1/tp1exo4test.py
+++
@@ -1,3 +0,0 @@
-print ( " veuillez saisir le nombre de minutes écoulé depuis le début du mois : " )
-minutes=int(input())
-print ( " il c'est passé" , minutes/60 , " heures soit " , minutes/60/24 , " jours " )
