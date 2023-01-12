3d824ca03b69f755f0e23b02204fe1296eb937e4
LakuzasS <104200455+LakuzasS@users.noreply.github.com>
2023-01-12 16:15:30 +0100

    Add files via upload

---
+++ tp5/ChaînesDeCharactères.py
@@ -0,0 +1,25 @@
+T = []
+voyelles = ['a','e','i','o','u','y']
+chaine = str(input("Ecrire du texte, 100 caractères max :"))
+longueur = 0
+compteur = 0
+pourcentage = 0
+occurrence = 0
+chaine2 = "wagon"
+if len(chaine) > 100:
+    str(input("Vous avez plus de 100 caractères, réésayez :"))
+for i in range(len(chaine)+1):
+    longueur = i
+print("La chaine contient",longueur,"caractères.")
+for j in range(0, len(chaine)-1):
+    for k in range(len(voyelles)):
+        if chaine[j] == voyelles[k]:
+            compteur += 1
+pourcentage = (compteur / longueur) * 100
+print(pourcentage)
+for a in range(len(chaine)):
+    if chaine2 == chaine[a]:
+        print("wagon est bien une sous-chaine de T et apparait la première fois à l'indice", a)
+    occurrence = chaine.count("wagon")
+print(occurrence)
+
