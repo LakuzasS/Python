4b46bb891c701b01ddf206075b24b50c7ad74b7e
LakuzasS <104200455+LakuzasS@users.noreply.github.com>
2023-01-16 15:58:11 +0100

    Update FicheDePaye.py

--- tp5/FicheDePaye.py
+++ tp5/FicheDePaye.py
@@ -3,10 +3,8 @@ salaire = int(input("Définir le salaire horaire :"))
 salaire_depart = salaire
 a = 0
 b = 0
-if heure < 160:
-    print("l'ouvrier n'a pas rempli ces heures de travail, il n'a pas de salaire")
-
-if heure == 160:
+if heure <= 160:
+    salaire *= heure
     print("l'ouvrier aura gagné",salaire,"€ en",heure,"heures")
 
 if heure >= 161 and heure <= 200:
